30cc2201d2826e824a5f525194d1ee2e3c0142bd
tandf <tandf@live.cn>
2019-10-30 08:52:35 -0400

    duplicate packets using python, but it's slow

--- bandwidth/generate_pcap.py
+++ bandwidth/generate_pcap.py
@@ -30,9 +30,23 @@ def random_port():
     return random.randint(0, 65535)
 
 
+def random_timeshift(time):
+    return random.uniform(0, time)
+
+
+def random_dup_pkt(pkt, add_c, add1, add2, p1, p2):
+    new_pkt = pkt.copy()
+    if pkt.src == add_c:
+        pkt[IP].src = add1
+        pkt[IP].dst = add2
+        pkt.sport = p1
+        pkt.dport = p2
+
+
 class FlowGenerater():
-    def __init__(self, start_time=time.time(), add_c=random_ip(),
+    def __init__(self, period, start_time=time.time(), add_c=random_ip(),
                  add_s=random_ip()):
+        self.period = period
         self.start_time = start_time
         self.add_c = add_c
         self.add_s = add_s
@@ -102,25 +116,46 @@ class FlowGenerater():
         self.seq_s_acc(len(data))
         return pkt
 
-    def generate_random_pkts(self, num, minLength, maxLength, time):
+    def generate_random_pkts(self, num, minLength, maxLength, retransmition):
         if maxLength == 0:
             maxLength = minLength + 1
         time_base = fg.pkts[-1].time
-        time_seq = generate_time_seq(num, time)
+        time_seq = generate_time_seq(num, self.period)
         for i in range(num):
             data = random_string(minLength, maxLength)
             funs = [self.server_pkt, self.client_pkt]
             pkt = random.choice(funs)(data)
             pkt.time = time_base + time_seq[i]
             self.pkts += [pkt]
-            if random.random() < 0.005:
+            if retransmition and random.random() < 0.005:
                 dup_pkt = pkt.copy()
                 dup_pkt.time += random.uniform(0.1, 0.3)
                 self.pkts += [dup_pkt]
 
+    def duplicate_flow(self, times):
+        if times <= 1:
+            return
+
+        packets = []
+        ip_port_map = [[random_ip(), random_ip(), random_port(), random_port(),
+                        random_timeshift(self.period)] for i in range(times)]
+        for pkt in self.pkts:
+            for ip_port in ip_port_map:
+                new_pkt = pkt.copy()
+                new_pkt.time += ip_port[4]
+                if pkt.src == self.add_c:
+                    [new_pkt[IP].src, new_pkt[IP].dst, new_pkt.sport,
+                     new_pkt.dport] = ip_port[:4]
+                else:
+                    [new_pkt[IP].dst, new_pkt[IP].src, new_pkt.dport,
+                     new_pkt.sport] = ip_port[:4]
+                packets += [new_pkt]
+
+        self.pkts = packets
+
 
 parser = argparse.ArgumentParser(description='Generate random flow.')
-parser.add_argument('-f', dest='fnum', default=10,
+parser.add_argument('-f', dest='fnum', default=2,
                     help='flow number', type=int)
 parser.add_argument('-p', dest='pnum', default=2,
                     help='packet number', type=int)
@@ -133,16 +168,23 @@ parser.add_argument('-t', dest='time', default=100,
                     help='flow time period', type=float)
 parser.add_argument('-o', dest='outfile', default='pcap/temp.pcapng',
                     help='output file path', type=str)
+parser.add_argument('-r', dest='retransmition', help='random retransmition',
+                    action="store_true")
+parser.add_argument('-d', dest='duplicate', default=2,
+                    help='duplicate flow', type=int)
 args = parser.parse_args()
 
 all_p = []
 start_time = time.time()
 for i in range(args.fnum):
-    fg = FlowGenerater(start_time=start_time+random.uniform(0, args.time))
+    fg = FlowGenerater(period=args.time,
+                       start_time=start_time+random_timeshift(args.time))
     fg.handshake_pkts()
     fg.generate_random_pkts(num=args.pnum, minLength=args.minLength,
-                            maxLength=args.maxLength, time=args.time)
+                            maxLength=args.maxLength,
+                            retransmition=args.retransmition)
     fg.finish_pkts()
+    fg.duplicate_flow(args.duplicate)
 
     all_p += fg.pkts
 
