c2dc2d9303e0c218d396fd85ad4f6c4b698f10e5
tandf <tandf@live.cn>
2019-11-19 18:50:00 +0800

    community regex rules

--- bandwidth/run.sh
+++ bandwidth/run.sh
@@ -4,11 +4,9 @@ files=$(ls rules)
 for file in $files ; do
     # Generate rules
     ./generate_rules.sh -p rules/$file
-    echo "alert tcp any any -> any any ( msg:"ddos"; detection_filter: track by_dst, count 5, seconds 100; flow:stateless; sid:200000000; )" >> test.rules
 
     # Log and test
-    filename="flows_d5_$file.log"
-    echo "threshhold: 5" > $filename
+    filename="flows_pattern_$file.log"
     echo "pattern file: $file" >> $filename
     ./flows.sh $filename
 done
